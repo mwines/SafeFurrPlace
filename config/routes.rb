ee7282c76102690588e757c6b403550e95d2fad8
Zach Moses <musicmanz93@hotmail.com>
2016-02-06 14:41:12 -0800

    Change root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'static#home'
+
   get 'static/home'
 
   get 'static/about'
@@ -7,10 +9,6 @@ Rails.application.routes.draw do
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  root 'static#home'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
